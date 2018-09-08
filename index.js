04d38cf6a4e8e803e51e95e5622ce2e116d5bb51
Jerry Peng <png.jrry@gmail.com>
2018-09-08 18:07:19 -0400

    debugging speech recognition

--- index.js
+++ index.js
@@ -84,8 +84,7 @@ app.post('/transcribe', type, function (req, res) {
                 .then(data => {
                     const response = data[0];
                     const transcription = response.results
-                        .map(result => result.alternatives[0].transcript)
-                        .join('\n');
+                    
                     console.log(`Transcription: ${transcription}`);
                 })
                 .catch(err => {
