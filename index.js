8eecbcde90b149708f99d1ce10c7c595f4b1d89d
Jerry Peng <png.jrry@gmail.com>
2018-09-08 18:52:44 -0400

    debugging speech recognition

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.post('/transcribe', type, function (req, res) {
 
     const gcsUri = `gs://${bucketName}/${req.file.filename}`;
     const encoding = 'OGG_OPUS';
-    const sampleRateHertz = 8000;
+    const sampleRateHertz = 12000;
     const languageCode = 'en-US';
 
     const config = {
