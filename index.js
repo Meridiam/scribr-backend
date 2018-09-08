5b857637f02b82f97ae043c172706b8471a50411
Jerry Peng <png.jrry@gmail.com>
2018-09-08 16:55:17 -0400

    reresh

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ var upload = multer({dest: __dirname + '/'});
 var type = upload.any();
 var bucketName = 'scribr-215805.appspot.com';
 
-app.post('/transcribe', function (req, res) {
+app.post('/transcribe', type, function (req, res) {
     var storage = new Storage({
         keyFilename: __dirname + '/config/scribr-215805-da49aa87d062.json'
     });
